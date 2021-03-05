b608fef9bf2a9c04d39ba2239f35d166a7979ee9
Joshua <legodude17@users.noreply.github.com>
2021-03-05 14:00:50 -0800

    Verb_Multishot

---
+++ 1.2/Source/FraudeconCode/VerbProps.cs
@@ -0,0 +1,13 @@
+﻿using HediffResourceFramework;
+
+// ReSharper disable InconsistentNaming
+
+namespace FraudeconCode
+{
+    public class VerbProps : VerbResourceProps
+    {
+        public float multishotRadius;
+        public int multishotShots;
+        public bool multishotTargetFriendly = false;
+    }
+}
