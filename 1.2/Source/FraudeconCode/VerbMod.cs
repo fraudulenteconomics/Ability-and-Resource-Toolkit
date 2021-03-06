6f1ed9b5302e37864827fc956769cddf8f15b226
Joshua <legodude17@users.noreply.github.com>
2021-03-05 22:50:36 -0800

    Verb_Graveblossom

---
+++ 1.2/Source/FraudeconCode/VerbMod.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace FraudeconCode
+{
+    public class VerbMod : Mod
+    {
+        public VerbMod(ModContentPack content) : base(content)
+        {
+            var harm = new Harmony("fradulenteconomics.verbs");
+            GraveblossomHelpers.DoPatches(harm);
+        }
+    }
+}
