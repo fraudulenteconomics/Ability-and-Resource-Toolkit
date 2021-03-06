9a4302ca1a4fd943ef3a71d681aeda3621953fbc
Joshua <legodude17@users.noreply.github.com>
2021-03-05 16:54:32 -0800

    Verb_Harvest

---
+++ 1.2/Source/FraudeconCode/Verb_AreaEffect.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+using Verse.AI;
+
+namespace FraudeconCode
+{
+    public abstract class Verb_AreaEffect : Verb_CastBase
+    {
+        public VerbProps Props => verbProps as VerbProps;
+
+        protected override bool TryCastShot()
+        {
+            foreach (var cell in GenRadial.RadialCellsAround(currentTarget.Cell, Props.effectRadius, true))
+                AffectCell(cell);
+            return true;
+        }
+
+        public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+        {
+            needLOSToCenter = false;
+            return Props.effectRadius;
+        }
+
+        protected abstract void AffectCell(IntVec3 cell);
+    }
+}
---
+++ 1.2/Source/FraudeconCode/Verb_Harvest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace FraudeconCode
+{
+    public class Verb_Harvest : Verb_AreaEffect
+    {
+        protected override void AffectCell(IntVec3 cell)
+        {
+            var plants = cell.GetThingList(caster.Map).OfType<Plant>().Where(p =>
+                p.def.plant.harvestedThingDef != null && p.HarvestableNow).ToList();
+            foreach (var plant in plants)
+            {
+                var thing = ThingMaker.MakeThing(plant.def.plant.harvestedThingDef);
+                thing.stackCount = (int) (plant.YieldNow() * Props.yieldMultiplier);
+
+                if (thing.stackCount <= 0) continue;
+                if (caster.Faction != Faction.OfPlayer) thing.SetForbidden(true);
+
+                Find.QuestManager.Notify_PlantHarvested(CasterPawn, thing);
+                CasterPawn?.records?.Increment(RecordDefOf.PlantsHarvested);
+                GenPlace.TryPlaceThing(thing, plant.Position, caster.Map, ThingPlaceMode.Near);
+                plant.def.plant.soundHarvestFinish?.PlayOneShot(caster);
+                plant.PlantCollected();
+            }
+        }
+    }
+}
