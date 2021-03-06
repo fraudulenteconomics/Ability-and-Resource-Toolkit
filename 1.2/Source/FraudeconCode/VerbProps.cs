231258638c45bc8ef0da0363e989f902c7a5d401
Joshua <legodude17@users.noreply.github.com>
2021-03-05 16:54:19 -0800

    Verb_Extinguish

--- 1.2/Source/FraudeconCode/VerbProps.cs
+++ 1.2/Source/FraudeconCode/VerbProps.cs
@@ -7,9 +7,13 @@ namespace FraudeconCode
 {
     public class VerbProps : VerbResourceProps
     {
+        public bool alwaysGetChunks = true;
         public DamageDef cauterizeDamageDef;
+        public float effectRadius;
+        public float extinguishRadius;
         public float multishotRadius;
         public int multishotShots;
         public bool multishotTargetFriendly = false;
+        public float yieldMultiplier = 1f;
     }
 }
