ed01a8c12350082419fcd2db02d7805a22b6248d
Joshua <legodude17@users.noreply.github.com>
2021-03-07 00:58:00 -0800

    Verb_Blinkstrike

---
+++ 1.2/Source/FraudeconCode/Verb_Blinkstrike.cs
@@ -0,0 +1,40 @@
+﻿using Verse;
+using Verse.AI;
+
+namespace FraudeconCode
+{
+    public class Verb_Blinkstrike : Verb_CastBase
+    {
+        protected override bool TryCastShot()
+        {
+            var cell = CurrentTarget.HasThing ? CurrentTarget.Thing.RandomAdjacentCellCardinal() : CurrentTarget.Cell;
+            var marker = (TeleMarker) GenSpawn.Spawn(ThingDef.Named("TeleportMarker"), caster.Position, caster.Map);
+            marker.Target = caster;
+            marker.EndTick = Find.TickManager.TicksGame + 300;
+            caster.Position = cell;
+            if (CasterIsPawn) CasterPawn.Notify_Teleported();
+            return true;
+        }
+    }
+
+    public class TeleMarker : Thing
+    {
+        public int EndTick;
+        public Thing Target;
+
+        public override void Tick()
+        {
+            if (EndTick > Find.TickManager.TicksGame) return;
+            Target.Position = Position;
+            if (Target is Pawn p) p.Notify_Teleported();
+            Destroy();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref EndTick, "endTick");
+            Scribe_References.Look(ref Target, "target");
+        }
+    }
+}
